e584c29497cc6c056862dec40fa7f73294c423e9
MohamedSaeed-dev <mohamedsas966@gmail.com>
2024-06-27 18:26:01 +0300

    third commit

--- server/src/index.js
+++ server/src/index.js
@@ -8,7 +8,7 @@ app.use(cors());
 const server = http.createServer(app);
 const io = new Server(server,{
     cors: {
-        origin: 'http://localhost:3000',
+        origin: '*',
         methods: ['GET', 'POST']
     }
 })
